3dbd080b3232c70f4c71c3f6d29397645d979c49
刘明霄 <lmx4422@ly.com>
2018-03-16 11:22:16 +0800

    --修改

--- producer/producer.go
+++ producer/producer.go
@@ -1,24 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
 	"os"
 	"strings"
 	"time"
-
-	_ "net/http/pprof"
+	"fmt"
 
 	"github.com/Shopify/sarama"
-	"github.com/sdbaiguanghe/glog"
 )
 
 func main() {
-	go func() {
-		log.Println(http.ListenAndServe("localhost:8088", nil))
-	}()
-
 	for {
 		producer()
 	}
@@ -26,34 +17,22 @@ func main() {
 
 func producer() {
 	config := sarama.NewConfig()
-	config.Producer.Return.Successes = true //必须有这个选项
-	config.Producer.Timeout = 5 * time.Second
-	p, err := sarama.NewAsyncProducer(strings.Split("10.100.159.117:9092", ","), config)
-	defer p.Close()
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Partitioner = sarama.NewRandomPartitioner
+	config.Producer.Return.Successes = true
+	msg := &sarama.ProducerMessage{}
+	msg.Topic = "apm-cdn"
+	msg.Partition = int32(-1)
+	msg.Key = sarama.StringEncoder("key")
+	msg.Value = sarama.ByteEncoder("你好, 世界!" + time.Now().Format("15:04:05"))
+	producer, err := sarama.NewSyncProducer(strings.Split("10.100.159.117:9092", ","), config)
 	if err != nil {
-		return
+		os.Exit(500)
 	}
-
-	//必须有这个匿名函数内容
-	go func(p sarama.AsyncProducer) {
-		errors := p.Errors()
-		success := p.Successes()
-		for {
-			select {
-			case err := <-errors:
-				if err != nil {
-					glog.Errorln(err)
-				}
-			case <-success:
-			}
-		}
-	}(p)
-
-	v := "测试" + time.Now().Format("15:04:05")
-	fmt.Fprintln(os.Stdout, v)
-	msg := &sarama.ProducerMessage{
-		Topic: "apm-cdn",
-		Value: sarama.ByteEncoder(v),
+	defer producer.Close()
+	partition, offset, err := producer.SendMessage(msg)
+	if err != nil {
+		os.Exit(500)
 	}
-	p.Input() <- msg
+	fmt.Printf("partition=%d, offset=%d\n", partition, offset)
 }
